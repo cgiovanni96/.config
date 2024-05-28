ca8ae41c52a7c9823cd67d81b065067bf45a1d49
Giovanni Costa <cgiovanni1996@gmail.com>
2024-05-28 14:32:15 +0200

    init

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+
+  {
+    "dinhhuy258/git.nvim",
+    event = "BufReadPre",
+    opts = {
+      keymaps = {
+        -- Open blame window
+        blame = "<Leader>gb",
+        -- Open file/folder in git repository
+        browse = "<Leader>go",
+      },
+    },
+  },
+
+  {
+    "echasnovski/mini.bufremove",
+    keys = {
+      { "<leader>bd", false },
+
+      {
+        "<leader>k",
+        function()
+          local bd = require("mini.bufremove").delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+            if choice == 1 then -- Yes
+              vim.cmd.write()
+              bd(0)
+            elseif choice == 2 then -- No
+              bd(0, true)
+            end
+          else
+            bd(0)
+          end
+        end,
+        desc = "Delete Buffer",
+      },
+      { "<leader>bD", false },
+    },
+  },
+
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+  },
+}
